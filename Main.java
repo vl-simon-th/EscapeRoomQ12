cefe4d4b06bcb73f3b91a07ea52514b4205180f2
Christoph <c@c.c>
2025-06-04 14:50:58 +0200

    uiuopioi

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main {
         System.out.println("Hello, World!");
         System.out.println("Hello vvvvvv :-)");
 
+        
         MainWindow window = new MainWindow();
         window.setVisible(true);
     }
