2b8605b80cc68af2d2a6ab25248079017fd63989
simon <simon@mthal.de>
2025-06-04 14:18:04 +0200

    Added MainWindow

---
+++ MainWindow.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class MainWindow extends JFrame {
+    public MainWindow() {
+        super("Main Window");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 300);
+        // Add components here
+    }
+}
