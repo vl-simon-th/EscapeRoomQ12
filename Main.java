dcefabbf9efd3a5b3d2ac4ae67b47bf1f4caad9d
Simon <ThalSi@o365.st-stephan.de>
2025-06-04 14:40:27 +0200

    First working stuff

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Hello vvvv :-)");
+        System.out.println("Hello vvvvv :-)");
 
         MainWindow window = new MainWindow();
         window.setVisible(true);
