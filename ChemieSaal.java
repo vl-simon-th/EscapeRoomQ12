9461e0dbb504bb9a5b00106a2387c47c1e9de3c5
Simon Thal <thalsi@o365.st-stephan.de>
2025-06-04 14:35:20 +0200

    first working stuff

--- ChemieSaal.java
+++ ChemieSaal.java
@@ -4,6 +4,6 @@ import javax.swing.JPanel;
 public class ChemieSaal extends JPanel {
     public ChemieSaal() {
         setBackground(Color.YELLOW);
-        setMaximumSize(new java.awt.Dimension(800, 600)); // Replace 800, 600 with your desired width and height
+        setMaximumSize(new java.awt.Dimension(100, 100)); // Replace 800, 600 with your desired width and height
     }
 }
