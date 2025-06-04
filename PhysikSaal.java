9cf3462c3061d0f32f3381c3379a026deed6e3a0
Christoph <c@c.c>
2025-06-04 15:05:34 +0200

    Physiksaal

---
+++ PhysikSaal.java
@@ -0,0 +1,8 @@
+import java.awt.Color;
+import javax.swing.JPanel;
+public class PhysikSaal extends JPanel {
+    public PhysikSaal() {
+        setBackground(Color.YELLOW);
+        setMaximumSize(new java.awt.Dimension(100, 100)); // Replace 800, 600 with your desired width and height
+    }
+}
