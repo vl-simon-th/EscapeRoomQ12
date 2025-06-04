ad788f7f7519d2a22e84537c588bd352b402ef35
Christoph <c@c.c>
2025-06-04 15:09:43 +0200

    Physiksaal

--- PhysikSaal.java
+++ PhysikSaal.java
@@ -1,7 +1,12 @@
 import java.awt.Color;
 import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
 public class PhysikSaal extends JPanel {
     public PhysikSaal() {
+        Image backround = Toolkit.getDefaultToolkit().createImage();
+        this.drawImage((background, 0, 0 null));
         setBackground(Color.YELLOW);
         setMaximumSize(new java.awt.Dimension(100, 100)); // Replace 800, 600 with your desired width and height
     }
