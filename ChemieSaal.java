324aae0e2dc327bfc5e8e6560e30627fe4c0036b
Simon Thal <thalsi@o365.st-stephan.de>
2025-06-04 14:31:26 +0200

    ChemieSaal Please enter the commit message for your changes. Lines starting

---
+++ ChemieSaal.java
@@ -0,0 +1,9 @@
+import java.awt.Color;
+import javax.swing.JPanel;
+
+public class ChemieSaal extends JPanel {
+    public ChemieSaal() {
+        setBackground(Color.YELLOW);
+        setMaximumSize(new java.awt.Dimension(800, 600)); // Replace 800, 600 with your desired width and height
+    }
+}
