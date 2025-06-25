11750348ae0d42fb233361194ad86b9d41f3baca
Simon <ThalSi@o365.st-stephan.de>
2025-06-25 14:51:37 +0200

    Map Update

--- PhysikSaal.java
+++ PhysikSaal.java
@@ -1,13 +1,5 @@
-import java.awt.Color;
-import javax.swing.JPanel;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.Toolkit;
-public class PhysikSaal extends JPanel {
+
+public class PhysikSaal {
     public PhysikSaal() {
-        Image backround = Toolkit.getDefaultToolkit().createImage();
-        this.drawImage((background, 0, 0 null));
-        setBackground(Color.YELLOW);
-        setMaximumSize(new java.awt.Dimension(100, 100)); // Replace 800, 600 with your desired width and height
     }
 }
